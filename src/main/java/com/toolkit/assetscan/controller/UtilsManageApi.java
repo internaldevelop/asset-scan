231bc3e7751d1addbc38b664afe57d9f82586d62
ytwei <249422@qq.com>
2019-04-25 13:36:08 +0800

    Add the mongodb's original data(exported, dbf format).

--- src/main/java/com/toolkit/assetscan/controller/UtilsManageApi.java
+++ src/main/java/com/toolkit/assetscan/controller/UtilsManageApi.java
@@ -1,5 +1,6 @@
 package com.toolkit.assetscan.controller;
 
+import com.alibaba.fastjson.JSONObject;
 import com.toolkit.assetscan.global.response.ResponseHelper;
 import com.toolkit.assetscan.global.utils.MyUtils;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -19,5 +20,18 @@ public class UtilsManageApi {
     @ResponseBody
     public String getUuid() {
         return MyUtils.generateUuid();
+    }
+
+    @RequestMapping(value = "/json-pretty-format", method = RequestMethod.POST)
+    @ResponseBody
+    public Object getJsonPrettyFormat(@RequestParam("input") String input) {
+        JSONObject jsonObject = JSONObject.parseObject(input);
+        return jsonObject;
+    }
+
+    @RequestMapping(value = "/xml2json", method = RequestMethod.POST)
+    @ResponseBody
+    public Object xml2JSON(@RequestParam("input") String input) {
+        return org.json.XML.toJSONObject(input);
     }
 }
