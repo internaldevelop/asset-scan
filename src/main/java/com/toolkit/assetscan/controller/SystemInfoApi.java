f4cb1b8e91b0bed7153fcbff561c2acd6cd45e99
ytwei <249422@qq.com>
2019-07-08 17:53:31 +0800

    add host & terminal system version info

--- src/main/java/com/toolkit/assetscan/controller/SystemInfoApi.java
+++ src/main/java/com/toolkit/assetscan/controller/SystemInfoApi.java
@@ -1,7 +1,10 @@
 package com.toolkit.assetscan.controller;
 
+import com.toolkit.assetscan.global.enumeration.ErrorCodeEnum;
+import com.toolkit.assetscan.global.response.ResponseHelper;
 import com.toolkit.assetscan.service.SystemInfoService;
 import io.swagger.annotations.Api;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 import com.alibaba.fastjson.JSONObject;
 
@@ -11,6 +14,8 @@ import com.alibaba.fastjson.JSONObject;
 @Api(value = "01. 系统信息接口", tags = "01-System Info API")
 public class SystemInfoApi {
     private final SystemInfoService systemInfoService;
+    @Autowired
+    private ResponseHelper responseHelper;
 
     public SystemInfoApi(SystemInfoService systemInfoService) {
         this.systemInfoService = systemInfoService;
@@ -21,7 +26,13 @@ public class SystemInfoApi {
      * @return
      */
     @RequestMapping(value = "/version", method = RequestMethod.GET)
-    public @ResponseBody Object getVersion() {
-        return systemInfoService.getSystemInfo();
+    public @ResponseBody Object getVersion(@RequestParam("sys_type") int systemType) {
+        if (systemType == 1) {
+            return systemInfoService.getHostSystemInfo();
+        } else if (systemType == 2) {
+            return systemInfoService.getTerminalSystemInfo();
+        }
+
+        return responseHelper.error(ErrorCodeEnum.ERROR_PARAMETER);
     }
 }
--- src/main/java/com/toolkit/assetscan/service/SystemInfoService.java
+++ src/main/java/com/toolkit/assetscan/service/SystemInfoService.java
@@ -13,9 +13,9 @@ public class SystemInfoService {
         this.responseHelper = responseHelper;
     }
 
-    public ResponseBean getSystemInfo() {
+    public ResponseBean getHostSystemInfo() {
         JSONObject sysInfo = new JSONObject();
-        sysInfo.put("sysName", "主站和终端系统自动化配置检测工具");
+        sysInfo.put("sysName", "主站系统自动化配置核查工具");
         sysInfo.put("desc", "根据基线要求，采用定制的安全核查策略，对各类设备及系统进行安全配置核查。");
         sysInfo.put("sysVer", "1.0.0.1001");
         sysInfo.put("copyright", "Copyright ©2019-2022 中国电科院");
@@ -26,4 +26,20 @@ public class SystemInfoService {
 
         return responseHelper.success(sysInfo);
     }
+
+    public ResponseBean getTerminalSystemInfo() {
+        JSONObject sysInfo = new JSONObject();
+        sysInfo.put("sysName", "终端系统自动化配置检测工具");
+        sysInfo.put("desc", "针对采集的 Linux 系统安全配置信息，匹配三级安全配置标准模板，提取出不符合规定的安全配置。");
+        sysInfo.put("sysVer", "1.0.0.201");
+        sysInfo.put("copyright", "Copyright ©2019-2022 中国电科院");
+        sysInfo.put("status", "运行中");
+        sysInfo.put("overview", "采用kali2 、Java、公司自主可控 MySQL 开发，系统实现智能电网操作系统配置扫描及检测，" +
+                "发现智能电网 Linux 系统的安全配置漏洞，完成智能电网 Linux 系统安全配置核查，并对检测进行定制。" +
+                "系统提供告警处理，并以邮件的方式通知用户。当系统发现Linux系统漏洞、Linux系统安全配置漏洞之后，" +
+                "将提供详细的报告，以邮件的方式通知用户。");
+
+        return responseHelper.success(sysInfo);
+    }
+
 }
